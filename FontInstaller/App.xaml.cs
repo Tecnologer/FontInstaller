0f516a85dd2793b4eb0191030bd52bbc8df6125d
tecnologer <rdominguez@tecnologer.net>
2019-03-21 22:48:12 -0700

    Single instance runnint at time

--- FontInstaller/App.xaml.cs
+++ FontInstaller/App.xaml.cs
@@ -7,6 +7,21 @@ namespace FontInstaller
     /// </summary>
     public partial class App : Application
     {
-        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (!SingleInstance.IsApplicationFirstInstance())
+            {
+                MessageBox.Show("Solo puede ejecutarse una instancia a la vez.", "Ya esta en ejecucion", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            SingleInstance.Release();
+            base.OnExit(e);
+        }
     }
 }
---
+++ FontInstaller/SingleInstance.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+
+namespace FontInstaller
+{
+    public class SingleInstance
+    {
+        private const string MutexName = "Tecnologer.FontInstaller";
+        private static bool firstApplicationInstance;
+        private static Mutex mutexApplication;
+
+        public static bool IsApplicationFirstInstance()
+        {
+            // Allow for multiple runs but only try and get the mutex once
+            if (mutexApplication == null)
+            {
+                mutexApplication = new Mutex(true, MutexName, out firstApplicationInstance);
+            }
+
+            return firstApplicationInstance;
+        }
+
+        public static void Release()
+        {
+            if (mutexApplication != null)
+            {
+                mutexApplication.Dispose();
+            }
+        }
+    }
+}
